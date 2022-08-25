5f6d64023ab52d8bded07a2ccd88dcd857de36d4
Tang Hoang Griep <businesshoangtang@gmail.com>
2022-08-26 00:37:54 +0200

    clean up and restructure code

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getComputerChoice(){
       return options[choice];
 }
 
-function playRound(e) {
+function getSelection(e) {
 
     // get computer selection
     let computerSelection = getComputerChoice();
@@ -22,47 +22,41 @@ function playRound(e) {
     else{
         playerSelection = "SCISSOR";
     }
-    
+
+    playRound(playerSelection, computerSelection);
+}    
+
+function playRound(playerSelection, computerSelection){
+
     // find the winner
     if(playerSelection.toUpperCase() === computerSelection.toUpperCase()){
         console.log("Tie!");
         return;
       }
+
+    // winning
     else if(
         (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSOR") || 
         (playerSelection.toUpperCase() === "SCISSOR" && computerSelection.toUpperCase() === "PAPER") ||
         (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK")){
+        
         playerPoints = playerPoints + 1;
         let message =  `${playerSelection} beats ${computerSelection}. You won! Score: ${playerPoints}:${computerPoints}`
-        
         console.log(message);
         return;
     }
-    else if(
-        (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER") || 
-        (playerSelection.toUpperCase() === "SCISSOR" && computerSelection.toUpperCase() === "ROCK") ||
-        (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSOR")){
+
+    // losing
+    else{
         computerPoints = computerPoints + 1;
         let message = `${playerSelection} loses to ${computerSelection}. You lost! Score: ${playerPoints}:${computerPoints}`
-        
         console.log(message);
         return;
     }
-}    
-
-/*
-function game(){
-    let roundLength = prompt("Choose the number of rounds.");
-    for(let i=0; i<roundLength; i++){
-        let playerSelection = prompt("Choose rock, paper or scissor.");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
 }
-*/
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
-    button.addEventListener('click', playRound)
+    button.addEventListener('click', getSelection)
 });
 
